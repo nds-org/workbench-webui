54bf1157360e53e23f3d8894d0a25f1b70c08646
Sara Lambert <lambert8@illinois.edu>
2022-09-24 01:57:40 -0500

    fix: block clicking save while saving/restart userapp, unique keys

--- src/views/EditService.tsx
+++ src/views/EditService.tsx
@@ -8,6 +8,7 @@ import {Button, Col, Form, FormControl, Nav, Row, Tab, Table, Tabs} from "react-
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faPlus, faSave} from "@fortawesome/free-solid-svg-icons";
 import {V1} from "../common";
+import {faSpinner} from "@fortawesome/free-solid-svg-icons/faSpinner";
 
 interface EnvVar {
     name: string;
@@ -24,6 +25,7 @@ function EditServicePage(props: {}) {
 
     const tabNames = ['Environment Variables'];
     const {stackId} = useParams<{ stackId: string; }>();
+    const [saving, setSaving] = useState(false);
     const [redirect, setRedirect] = useState('');
     const [userApp, setUserApp] = useState<V1.Stack>();
     const [key, setKey] = useState<string>('0');
@@ -106,6 +108,7 @@ function EditServicePage(props: {}) {
     };
 
     const save = () => {
+        setSaving(true);
         userApp?.services?.forEach(svc => {
             const svcEnv = vars.find(s => s.service === svc.service);
             svc.config = {};
@@ -113,11 +116,14 @@ function EditServicePage(props: {}) {
             V1.UserAppService.updateUserapp(stackId, userApp).then((updated) => {
                 console.log("UserApp saved: ", updated);
                 setRedirect('/my-apps');
+            }).finally(() => {
+                setSaving(false);
             });
         });
     }
 
     const disabled = (): boolean => {
+        if (saving) return true;
         if (!userApp?.services) {
             return false;
         }
@@ -146,48 +152,49 @@ function EditServicePage(props: {}) {
         <h2>Edit UserApp: {stackId}</h2>
         <Tabs id={'outerTabBar'} activeKey={key} onSelect={(e) => e && setKey(e)} fill>
             {
-                userApp?.services?.map((svc, index) => <Tab title={svc.service} eventKey={svc.service}>
-                    <Tab.Container id={'innerTabBar'} activeKey={innerKey} onSelect={(e) => e && setInnerKey(e)}>
-                        <Row className={'marginTop'}>
-                            <Col sm={3}>
+                userApp?.services?.map((svc, index) => <Tab title={svc.service} key={svc.service+"-svc-"+index} eventKey={svc.service}>
+                    <Tab.Container id={'innerTabBar'} key={innerKey} activeKey={innerKey} onSelect={(e) => e && setInnerKey(e)}>
+                        <Row key={'env-row-0'} className={'marginTop'}>
+                            <Col key={'env-col-0'} sm={3}>
                                 <Nav variant="pills" className="flex-column">
                                     <Nav.Item>
-                                        {tabNames.map(name => <Nav.Link eventKey={name}>{name}</Nav.Link>)}
+                                        {tabNames.map(name => <Nav.Link key={name} eventKey={name}>{name}</Nav.Link>)}
                                     </Nav.Item>
                                 </Nav>
                             </Col>
-                            <Col sm={9}>
+                            <Col key={'env-col-1'} sm={9}>
                                 <Tab.Content>
                                     <Tab.Pane eventKey="Environment Variables">
                                         <Form>
                                             <h4>Environment: {svc.service}</h4>
                                             {
-                                                vars.filter(s => svc.service === s.service).map(sv => <><Table striped bordered size={'sm'}>
-                                                    {
-                                                        sv?.env?.map((e, envIndex) => <tr>
-                                                            <td>
-                                                                <Form.Group controlId="envName">
-                                                                    <Form.Control required id={'envName'} name={'envName'} type="text" placeholder="Name is required" value={e.name} onChange={(e) => handleChange(e, svc.service, envIndex, 'name')} />
-                                                                </Form.Group>
-                                                            </td>
-                                                            <td>
-                                                                <Form.Group controlId="envVal">
-                                                                    <Form.Control type="text" id={'envValue'} name={'envValue'} placeholder="Enter a value (optional)" value={e.value} onChange={(e) => handleChange(e, svc.service, envIndex, 'value')} />
-                                                                </Form.Group>
-                                                            </td>
-                                                        </tr>)
-                                                    }
+                                                vars.filter(s => svc.service === s.service).map((sv, svcIndex) => <span key={svc.service+"-var-"+svcIndex}>
+                                                    <Table size={'sm'}>
+                                                        <tbody>
+                                                        {
+                                                            sv?.env?.map((e, envIndex) => <tr key={svc.service+"-env-"+envIndex} >
+                                                                <td>
+                                                                    <Form.Group controlId="envName">
+                                                                        <Form.Control required name={'envName'} type="text" placeholder="Name is required" value={e.name} onChange={(e) => handleChange(e, svc.service, envIndex, 'name')} />
+                                                                    </Form.Group>
+                                                                </td>
+                                                                <td>
+                                                                    <Form.Group controlId="envVal">
+                                                                        <Form.Control type="text" name={'envValue'} placeholder="Enter a value (optional)" value={e.value} onChange={(e) => handleChange(e, svc.service, envIndex, 'value')} />
+                                                                    </Form.Group>
+                                                                </td>
+                                                            </tr>)
+                                                        }
+                                                        </tbody>
                                                     </Table>
+
                                                     Total: {sv?.env?.length}
-                                                </>)
-                                            }
+                                                </span>)
+                                        }
                                         </Form>
 
                                         <Button onClick={() => addVar(svc.service)}><FontAwesomeIcon icon={faPlus} /></Button>
-                                        <Button disabled={disabled()} className={'btn-success'} onClick={save}><FontAwesomeIcon icon={faSave} /></Button>
-                                    </Tab.Pane>
-                                    <Tab.Pane eventKey="second">
-                                        Second!
+                                        <Button disabled={disabled()} className={'btn-success'} onClick={save}><FontAwesomeIcon className={saving ? 'fa-spin' : ''} icon={saving ? faSpinner : faSave} /></Button>
                                     </Tab.Pane>
                                 </Tab.Content>
                             </Col>
