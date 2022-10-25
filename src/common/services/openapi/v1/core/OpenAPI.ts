e578b7f3753324f43c879419458ab6b30e99dc12
Sara Lambert <lambert8@illinois.edu>
2022-10-25 14:21:30 -0500

    Sync OpenAPI generated client

--- src/common/services/openapi/v1/core/OpenAPI.ts
+++ src/common/services/openapi/v1/core/OpenAPI.ts
@@ -18,7 +18,7 @@ type Config = {
 
 export const OpenAPI: Config = {
     BASE: '/api/v1',
-    VERSION: '1.3.1',
+    VERSION: '2.0.0',
     WITH_CREDENTIALS: false,
     TOKEN: undefined,
     USERNAME: undefined,
