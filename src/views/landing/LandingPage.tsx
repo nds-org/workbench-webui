a12ed1871c0b8c4c0fd45ba6ebb072389a42ff40
Sara Lambert <lambert8@illinois.edu>
2022-10-27 00:31:22 -0500

    fix: rework LandingPage entirely

---
+++ src/views/landing/LandingPage.tsx
@@ -0,0 +1,78 @@
+import {Footer} from "../../common";
+import {useEffect, useState} from "react";
+import Button from "react-bootstrap/Button";
+import {Redirect} from "react-router-dom";
+
+import './LandingPage.css';
+import styled from "styled-components";
+import {useSelector} from "react-redux";
+import ReactGA from "react-ga";
+import LoginBanner from "./LoginBanner";
+import LandingSection from "./LandingSection";
+
+const LandingPage = () => {
+    const [redirect, setRedirect] = useState('');
+    const env = useSelector((state: any) => state.env);
+
+    useEffect(() => {
+        if (env?.analytics_tracking_id) {
+            ReactGA.pageview('/');
+        }
+    }, [env?.analytics_tracking_id]);
+
+
+    return (
+        redirect ?
+            <Redirect to={redirect} />
+            :
+        <>
+            <div className="login area row">
+                <LoginBanner />
+            </div>
+
+            <div className="container sections">
+                <div id="sectionListHeader" className="row">
+                    <div className="col">
+                        <h2>How It Works...</h2>
+                    </div>
+                </div>
+
+                <div id="sectionOne">
+                    <LandingSection numeral={'01'} header={env?.customization?.landing_header_1} body={env?.customization?.landing_section_1} diagram={'/landing-diagram-1.svg'} textFirst={true}></LandingSection>
+                </div>
+
+
+                <div id="sectionTwo">
+                    <LandingSection numeral={'02'} header={env?.customization?.landing_header_2} body={env?.customization?.landing_section_2} diagram={'/landing-diagram-2.svg'} textFirst={false}></LandingSection>
+                </div>
+
+                <div id="visitAllApps" className="row justify-content-md-center">
+                    <div className="col">
+                        <Button className="btn btn-dark btn-lg" onClick={() => setRedirect('/all-apps')}>Visit All the Apps</Button>
+                    </div>
+                </div>
+
+                {
+                    env?.support_email && <>
+                        <div id="contactUs" className="row justify-content-md-center">
+                            <div className="col-9">
+                                <div className="card text-dark bg-warning">
+                                    <div className="card-body" style={{ marginTop: "150px" }}>
+                                        <h3 className="card-title">Have Issues With Your Service?</h3>
+                                        <a href={ 'mailto:' + env?.support_email } className="btn btn-dark btn-lg">Contact Us</a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </>
+                }
+            </div>
+
+            <div style={{ height: "50px" }}></div>
+
+            <Footer></Footer>
+        </>
+    );
+}
+
+export default LandingPage;
---
+++ src/views/landing/LoginBanner.tsx
@@ -0,0 +1,44 @@
+import Button from "react-bootstrap/Button";
+import {useSelector} from "react-redux";
+import {Redirect} from "react-router-dom";
+import {useState} from "react";
+import  './LoginBanner.css'
+
+const LoginBanner = () => {
+    const [redirect, setRedirect] = useState('');
+    const env = useSelector((state: any) => state.env);
+
+    const gotoLogin = () => {
+        if (env?.signin_url) {
+            window.location.href = env?.signin_url;
+        } else {
+            setRedirect("/login")
+        }
+    }
+
+    return (
+        redirect ? <Redirect to={redirect} /> : <>
+            <div className="thumbnail text-center">
+                <img src="/login-banner.jpg" className="img-fluid" alt="..." />
+                <div className="caption">
+                    <h1 className="ui white header">
+                        {
+                            (env?.customization?.landing_html &&
+                                <span dangerouslySetInnerHTML={{__html: env?.customization?.landing_html + ""}}></span>) || <>
+                                <p className="nomargin">Labs Workbench allows developers to</p>
+                                <p className="nomargin">prototype tools that help build out the</p>
+                                <p className="nomargin">NDS framework and services.</p>
+                            </>
+                        }
+                    </h1>
+
+                    <Button style={{padding: "10px 30px"}} variant="light" size={'lg'} onClick={() => gotoLogin()}>
+                        Log In
+                    </Button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default LoginBanner;
