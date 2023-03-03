d5f30061f5862d3afb51720f898748a94b06fc8c
Sara Lambert <lambert8@illinois.edu>
2023-03-03 17:37:08 -0600

    fix: remove bottom margin from jumbotron in SearchCatalogHeader

--- src/views/all-apps/SearchCatalogHeader.tsx
+++ src/views/all-apps/SearchCatalogHeader.tsx
@@ -1,25 +1,16 @@
 import Input from "@material-ui/core/Input";
 import {useSelector} from "react-redux";
 
-const css = `
-.jumbotron {
-  padding: 2rem 1rem;
-  margin-bottom: 2rem;
-  background-color: #e9ecef;
-  border-radius: .3rem;
-}
-`;
-
 const SearchCatalogHeader = (props: { searchQuery: string; setSearchQuery: Function; }) => {
     const darkThemeEnabled = useSelector((state: any) => state.preferences.darkThemeEnabled);
     const env = useSelector((state: any) => state.env);
 
     return (
         <>
-            <style>{css}</style>
             <div className='jumbotron bg-banner' style={{
                 width: "100%",
                 color: "white",
+                marginBottom: 0,
                 backgroundImage: darkThemeEnabled ? "linear-gradient(rgba(100, 100, 0, 0.35),rgba(100, 100, 0, 0.35)),url('/catalog-banner.jpg')" : "linear-gradient(rgba(200, 0, 0, 0.35),rgba(200, 0, 0, 0.35)),url('/catalog-banner.jpg')",
                 backgroundSize: "1800px 500px",
                 backgroundRepeat: "round",
